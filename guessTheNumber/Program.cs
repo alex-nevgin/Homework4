b216fc25cfe2056dca52c473b91b01828f0785ee
Alexander Nevgin <alexblackstar86@gmail.com>
2021-10-20 20:47:56 +0300

    Homework4 Started

---
+++ guessTheNumber/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace guessTheNumber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ randomMatrix/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace randomMatrix
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
