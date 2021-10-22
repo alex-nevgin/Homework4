6020c717307d098e69a7e3d0bd83dc3113406b86
Alexander Nevgin <alexblackstar86@gmail.com>
2021-10-22 22:28:48 +0300

    Added randomMatrix

--- randomMatrix/Program.cs
+++ randomMatrix/Program.cs
@@ -1,9 +1,47 @@
-﻿namespace randomMatrix
+﻿using System;
+namespace randomMatrix
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            int[,] matrix;
+            string hintRows = "Введите число строк: ";
+            string hintColumns = "Введите число столбцов: ";
+            int r, c; // количество строк и столбцов
+            bool flagR, flagC; // флаги для проверки ввода
+           
+
+            Console.Write(hintRows);
+            // flagR = int.TryParse(Console.ReadLine(), out r);
+            // if (flagR == false)
+            // {
+            //     Console.WriteLine("Ошибка: число строк массива может быть только целым числом");
+            // }
+            r = Int32.Parse(Console.ReadLine());
+            
+            Console.Write(hintColumns);
+            // flagC = int.TryParse(Console.ReadLine(), out c);
+            // if (flagC == false)
+            // {
+            //     Console.WriteLine("Ошибка: число строк массива может быть только целым числом");
+            // }
+            c = Int32.Parse(Console.ReadLine());
+            
+            
+            Random rnd = new Random();
+            
+            matrix = new int[r,c];
+            
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    matrix[i, j] = rnd.Next(10);
+                    Console.Write($"{matrix[i, j]} ");
+                }
+            }
+
             
         }
     }
